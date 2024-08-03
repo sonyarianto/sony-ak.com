7e75984d9b320033136ad5fc1ab06fe2c8f7a98a
Sony AK <sony@sony-ak.com>
2024-08-03 18:47:34 +0700

    chore: Update npm packages and configuration files

--- web/next.js/src/app/page.tsx
+++ web/next.js/src/app/page.tsx
@@ -1,113 +1,139 @@
-import Image from "next/image";
-
+/* eslint-disable @next/next/no-img-element */
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white lg:static lg:size-auto lg:bg-none dark:from-black dark:via-black">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+      <img
+        src="https://play.tailwindcss.com/img/beams.jpg"
+        alt=""
+        className="absolute left-1/2 top-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
+        width="1308"
+      />
+      <div className="absolute inset-0 bg-[url(https://play.tailwindcss.com/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
+      <div className="relative bg-white px-6 pb-8 pt-10 shadow-md ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+        <div className="mx-auto max-w-md">
+          <img
+            src="https://www.npmjs.com/npm-avatar/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXJVUkwiOiJodHRwczovL3MuZ3JhdmF0YXIuY29tL2F2YXRhci84MjY0ODJlMzBmMmJmMDAxNTk2ZjZlYjkwODA4MjdjMz9zaXplPTQ5NiZkZWZhdWx0PXJldHJvIn0.ANFeUy_WPf1KrjHbaEIXRPdhekzE_PhlNU6j8PTVsuc"
+            alt="Sony AK"
+            className="block h-[88px] w-[88px] rounded-lg border-[3px] border-solid border-slate-300 object-cover"
+          />
+          <h1 className="rainbow-based-on-hellonext mt-1 bg-clip-text text-2xl font-extrabold text-gray-800 text-transparent">
+            Sony AK
+          </h1>
+          <p className="text-sm text-gray-600">Member of Technical Staff</p>
+          <div className="divide-y divide-gray-300/50">
+            <div className="space-y-6 py-8 text-base leading-7 text-gray-600">
+              <ul className="space-y-2">
+                <li className="flex items-center">
+                  <a href="https://github.com/sonyarianto" target="_blank">
+                    <img
+                      src="/assets/images/github.svg"
+                      alt="GitHub"
+                      className="h-6 w-6"
+                    />
+                  </a>
+                  <a href="https://github.com/sonyarianto" target="_blank">
+                    <p className="ml-4">
+                      GitHub,{" "}
+                      <span className="text-purple-600">
+                        educational materials.
+                      </span>
+                    </p>
+                  </a>
+                </li>
+                <li className="flex items-center">
+                  <a href="https://youtube.com/@bljdev" target="_blank">
+                    <img
+                      src="/assets/images/youtube.svg"
+                      alt="YouTube"
+                      className="h-6 w-6"
+                    />
+                  </a>
+                  <a href="https://youtube.com/@bljdev" target="_blank">
+                    <p className="ml-4">
+                      BLJ,{" "}
+                      <span className="text-orange-600">
+                        educational materials.
+                      </span>
+                    </p>
+                  </a>
+                </li>
+                {/* <li className="flex items-center">
+                  <a href="https://youtube.com/@adayangtanya" target="_blank">
+                    <img
+                      src="/assets/images/youtube.svg"
+                      alt="YouTube"
+                      className="h-6 w-6"
+                    />
+                  </a>
+                  <a href="https://youtube.com/@adayangtanya" target="_blank">
+                    <p className="ml-4">
+                      ada yang tanya,{" "}
+                      <span className="text-emerald-600">
+                        in Bahasa Indonesia.
+                      </span>
+                    </p>
+                  </a>
+                </li> */}
+                <li className="flex items-center">
+                  <a
+                    href="https://www.google.com/search?q=Sony+AK+vs+Sony+corporation"
+                    target="_blank"
+                  >
+                    <img
+                      src="/assets/images/justice.svg"
+                      alt="Google"
+                      className="h-6 w-6"
+                    />
+                  </a>
+                  <a
+                    href="https://www.google.com/search?q=Sony+AK+vs+Sony+corporation"
+                    target="_blank"
+                  >
+                    <p className="ml-4">
+                      case study,{" "}
+                      <span className="text-sky-600">
+                        my domain vs Sony Corp.
+                      </span>
+                    </p>
+                  </a>
+                </li>
+                <li className="flex items-center">
+                  <a href="https://buka.sh" target="_blank">
+                    <img
+                      src="/assets/images/buka.svg"
+                      alt="Buka"
+                      className="h-6 w-6"
+                    />
+                  </a>
+                  <a href="https://buka.sh" target="_blank">
+                    <p className="ml-4">
+                      Buka,{" "}
+                      <span className="text-emerald-600">
+                        something that you open everyday.
+                      </span>
+                    </p>
+                  </a>
+                </li>
+              </ul>
+              <p>
+                Keep learning, stay relevant, relevancy increases value.
+                Comfortable with being uncomfortable.
+              </p>
+              <p className="text-sm text-gray-400">
+                &copy; 2003 - {new Date().getFullYear()} by Sony Arianto
+                Kurniawan - All rights reserved. Design based on{" "}
+                <a href="https://play.tailwindcss.com" target="_blank">
+                  play.tailwindcss.com
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="relative mt-3 w-full px-3 text-center text-xs text-gray-400 sm:mx-auto sm:max-w-lg">
+        This website is administrated by Sony Arianto Kurniawan and not related
+        to Sony Corporation or its affiliate at all.
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
