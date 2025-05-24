ac5d90b82cbde25879020d1a9f69a85076c9a5cf
Sony AK <sony@sony-ak.com>
2025-05-24 23:51:20 +0700

    fix: update YouTube link to include subscription confirmation

--- web/next.js/src/app/page.tsx
+++ web/next.js/src/app/page.tsx
@@ -69,7 +69,7 @@ export default function Home() {
                       <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
                     </svg>
                   </a>
-                  <a href="https://youtube.com/@sonyarianto" target="_blank">
+                  <a href="https://youtube.com/@sonyarianto?sub_confirmation=1" target="_blank">
                     <p className="ml-4">
                       YouTube channel,{" "}
                       <span className="text-orange-600">random things.</span>
